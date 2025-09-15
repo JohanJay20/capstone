e638bd3e7ea10eba199e7833280fa5d82fc639ae
JohanJay20 <2101104208@student.buksu.edu.ph>
2025-09-15 23:23:07 +0800

    Update sendEmail.js

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -4,12 +4,15 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true, // true for port 465
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
 });
+
 
 export default async function sendEmail(to, password) {
   await transporter.sendMail({
